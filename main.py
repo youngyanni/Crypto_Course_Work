b8ec56f9aca258233b011144a8a913966ec488f9
youngyanni <87753697+youngyanni@users.noreply.github.com>
2022-11-05 18:51:07 +0300

    1 Task

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+
+#ЧЕРНЫШЯН ЮРЬЕВИЧ
+#1101 0111 1100 0101 1101 0000 1100 1101
+#1101 1011 1101 1000 1101 1111 1100 1101
+#После перестановки
+#(1111 1111) (0111 0101) (1
+def
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
